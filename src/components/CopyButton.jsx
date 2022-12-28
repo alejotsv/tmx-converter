76a7ef1a7ea45c3c0ce9a188b553ff153b348401
Alejandro Salas <asalas.wd@outlook.com>
2022-12-27 22:07:38 -0500

    complete functionality to copy all segments

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 const CopyButton = () => {
+  const copyAll = () => {
+    console.log(document.getElementsByClassName('tmx-table'));    
+    const table = document.getElementsByClassName('tmx-table')[0];
+    const range = document. createRange();
+    range.selectNodeContents(table);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    document.execCommand('copy');
+  }
+
   return(
-    <button>
+    <button onClick={copyAll}>
       Copy All
     </button>
   )
