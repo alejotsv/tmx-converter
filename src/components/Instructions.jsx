2e2e7443f1186cb9b322037d9368539ffe19e755
Alejandro Salas <asalas.wd@outlook.com>
2022-12-28 20:42:19 -0500

    add style to instructions card

--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const Instructions = () => {
-  return(
-    <div className='instructions'>
-      <h1>Upload a TMX file and display segments as a table</h1>
-      <h2>Instructions:</h2>
-      <ol>
-        <li>Click on the 'Choose file' button</li>
-        <li>Select a TMX file; make sure it has the correct extension (.tmx)</li>
-        <li>Click on 'Show segments'</li>
-        <li>Once the segments are displaying, simply click on 'Copy All' to copy all segments in a format ready to be pasted in an Excel file</li>
-        <li>To upload another file, click on 'Upload another file'</li>
-      </ol>
-    </div>
+  return(    
+    <Card className="instructions">
+      <Card.Header className="text-center">Upload a TMX file and display segments as a table</Card.Header>
+      <Card.Body>        
+        <Card.Text>
+          <ol>
+            <li>Click on the 'Choose file' button</li>
+            <li>Select a TMX file; make sure it has the correct extension (.tmx)</li>
+            <li>Click on 'Show segments'</li>
+            <li>Once the segments are displaying, simply click on 'Copy All' to copy all segments in a format ready to be pasted in an Excel file</li>
+            <li>To upload another file, click on 'Upload another file'</li>
+         </ol>
+        </Card.Text>
+        
+      </Card.Body>      
+    </Card>          
   )
 }
 
