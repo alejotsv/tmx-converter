63d2e85affb0c78bab82b05f5fe0f5878aa9c876
Alejandro Salas <asalas.wd@outlook.com>
2023-01-15 23:54:41 -0500

    add modal to Generate CSV

--- src/components/CSVButton.jsx
+++ src/components/CSVButton.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { TextEncoder } from 'text-encoding';
 
 const CSVButton = ({ arr }) => {   
   const [url, setUrl] = useState();
   const [showModal, setShowModal] = useState(false);
 
-  const handleClose = () => setShowModal(false);
+  const handleClose = () => {
+    setShowModal(false);
+    createCSV();
+  }
+  
   const handleShow = () => setShowModal(true);
 
 
@@ -52,7 +57,22 @@ const CSVButton = ({ arr }) => {
 
   return(
     <div className='btn-area'>      
-      <Button variant='success' onClick={createCSV} >Generate CSV</Button>
+      {/* <Button variant='success' onClick={createCSV} >Generate CSV</Button> */}
+      <Button variant='success' onClick={handleShow} >Generate CSV</Button>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Select minimum number of characters per segment</p>
+        </Modal.Body>
+        <Modal.Footer>          
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       { url && <a href={url} download='data.csv'>Download CSV file</a> }      
     </div>    
   )
